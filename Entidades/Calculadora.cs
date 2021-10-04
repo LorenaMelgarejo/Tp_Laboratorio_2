34276644a37036a35674ab3ecd0e73907103ec0c
lorena Melgarejo <lorena.melgarejo.n@gmail.com>
2021-10-04 00:37:55 -0300

    TP_02

---
+++ Entidades/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        public double Operar(Operando num1, Operando num2, char operador)
+        {
+            double resultado = 0;
+            string operadores;
+            operadores = ValidarOperador(operador);
+
+            switch (operadores)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+            }
+            return resultado;
+        }
+
+        private static char ValidarOperador(char operador)
+        {
+            while (operador == '+' || operador == '-' || operador == '*' || operador == '/')
+            {
+                return operador;
+
+            }
+            return '+';
+        }
+    }
+}
---
+++ TP-02/Entidades/Ciclomotor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color):base(chasis,marca,color)
+        {
+        }
+        
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+
+        /// <summary>
+        /// Mostrara todos los datos de ciclomotor
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("TAMAÑO : " + this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
